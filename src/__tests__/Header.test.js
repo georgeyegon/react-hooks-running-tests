bde5998372791dd1418657a5b5a642f515fa5058
George Yegon <georgeyegon67@gmail.com>
2024-10-31 12:26:53 +0300

    initial commit

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -6,6 +6,9 @@ import Header from "../components/Header";
 
 test("displays the text 'hello from the Header!'", () => {
   render(<Header />);
-
+  console.log(screen.queryByText("hello from the Header!"));
   expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
 });
+
+
+
